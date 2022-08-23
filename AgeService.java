a1bb343374fb03ec8503b27dcb0fc4975d4d3ca1
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-08-23 17:53:22 +0530

    Create AgeService.java

---
+++ AgeService.java
@@ -0,0 +1,24 @@
+//AgeCalculator/src/main/java/com/cognizant/AgeService.java
+     package com.cognizant;
+     
+     import java.time.LocalDate;
+     import java.time.Period;
+     import java.time.format.DateTimeFormatter;
+     
+     public class AgeService {
+     
+     	
+    	public  String calculateAge (String date){
+    	
+    	//add the code as per the requirement
+    	 LocalDate localDate = LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            Period period = Period.between(localDate, LocalDate.now());
+            int years = period.getYears();
+            int months = period.getMonths();
+            int days = period.getDays();
+    
+            return "you are " + years + " years, " + months + " months, " + days + " days old.";
+    	
+    }
+    }
+   
